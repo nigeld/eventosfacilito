918a5da021d0683fb54c2169d6511e081d3a5efe
Nigel Davila <nayeldavila@gmail.com>
2017-03-18 21:59:31 -0500

    Servicios para validar login

--- app/app.module.ts
+++ app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from "./login/login.component";
 import { SignUpComponent } from "./signup/signup.component";
 
 import { AuthGuard } from "./guards/auth.guard";
+import { AuthService } from "./services/auth.service";
 
 @NgModule({
     bootstrap: [
@@ -34,7 +35,8 @@ import { AuthGuard } from "./guards/auth.guard";
     ],
     providers: [
         ItemService,
-        AuthGuard
+        AuthGuard,
+        AuthService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
